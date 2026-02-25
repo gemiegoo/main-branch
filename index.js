557a480bb4e0e311f1954ddeeaad44f8d75b2922
gemiegoo <gemini20251609@gmail.com>
2026-02-24 17:29:01 -0800

    Limpieza total y arranque de Glawbot

---
+++ index.js
@@ -0,0 +1,24 @@
+﻿const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+require('dotenv').config();
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    console.log('--- AGENTE GLAWBOT: ESCANEA PARA CONTROL TOTAL ---');
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log(' GLAWBOT-CORE OPERATIVO EN WINDEV');
+});
+
+client.on('message', async msg => {
+    if (msg.body === '!status') {
+        msg.reply(' Agente Glawbot en línea.\n Sistema: WinDev VM\n Google Cloud: Conectado');
+    }
+});
+
+client.initialize();
